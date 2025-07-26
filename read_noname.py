d5e093021ec48ee39b3f769021f58baf22181972
ehnooi <t.yoon.268@gmail.com>
2025-07-26 13:26:27 +0900

    "New_Update"

--- read_noname.py
+++
@@ -1,5 +0,0 @@
-import codecs
-
-f = codecs.open('C:\\banghak\\noname.txt', 'r', encoding='cp949')
-print(f.read())
-f.close()
--- read_noname_to_file.py
+++
@@ -1,7 +0,0 @@
-import codecs
-
-f_in = codecs.open(r'C:\\banghak\\noname.txt', 'r', encoding='cp949')
-f_out = open(r'C:\\banghak\\temp_noname_content.txt', 'w', encoding='utf-8')
-f_out.write(f_in.read())
-f_in.close()
-f_out.close()
