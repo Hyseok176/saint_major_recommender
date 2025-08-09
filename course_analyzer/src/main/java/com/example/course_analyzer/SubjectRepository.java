7c64659b9aaefdd2172f75f4441ad67d154b6ed6
Hyseok176 <gilmon008@gmail.com>
2025-08-09 17:47:01 +0900

    refactor: Clean up database entities and logging
    - Remove unused Subject entity and its corresponding repository.
    
    - Disable SQL query logging in the dev profile to prevent excessive console output.

--- course_analyzer/src/main/java/com/example/course_analyzer/SubjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.course_analyzer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface SubjectRepository extends JpaRepository<Subject, String> {
-}
