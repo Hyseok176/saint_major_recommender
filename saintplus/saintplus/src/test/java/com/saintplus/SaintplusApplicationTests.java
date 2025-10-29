85bf5e3a6cd05355128e45ece9ae18b0f60bd86a
Hyseok176 <gilmon008@gmail.com>
2025-10-29 20:47:04 +0900

    controller부터

---
+++ saintplus/saintplus/src/test/java/com/saintplus/SaintplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saintplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaintplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
