97a117090350c358b465fc69723e3ab26b3a5efd
Hyseok176 <gilmon008@gmail.com>
2025-11-19 20:42:03 +0900

    프로젝트 환경 설정

---
+++ saintplus/saintplus/src/main/java/com/saintplus/dto/AiRecommendationDto.java
@@ -0,0 +1,13 @@
+package com.saintplus.dto;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class AiRecommendationDto {
+    private Long user_id;
+    private List<String> recommendations; // Python이 주는 과목 리스트
+}
